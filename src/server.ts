50339b0f7e70527a87042d176398a59c0b59a995
Antonio-Lima <gutto1998@gmail.com>
2023-07-01 21:56:08 -0300

    File created for transaction routes.

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,13 @@
 import crypto from "node:crypto";
 
 import fastify from "fastify";
-import { knex } from "./database";
 import { env } from "./env";
+import { transactionRoutes } from "./routes/transaction";
 
 const app = fastify();
 
+app.register(transactionRoutes);
+
 app.listen({ port: env.PORT }).then(() => {
   console.log("HTTP server is running");
-});
-
-app.get("/hello", async () => {
-  const transaction = await knex("transactions").select("*");
-
-  return transaction;
 });
