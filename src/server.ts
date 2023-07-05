1835f2c1edaae61be4acedfca2cb865662afe891
AntonioLima <gutto1998@gmail.com>
2023-07-04 21:16:09 -0300

    Registered cookies.

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import crypto from "node:crypto";
-
 import fastify from "fastify";
+import cookie from "@fastify/cookie";
 import { env } from "./env";
 import { transactionRoutes } from "./routes/transaction";
 
 const app = fastify();
+
+app.register(cookie);
 
 app.register(transactionRoutes, {
   prefix: "transactions",
